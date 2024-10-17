57edf4a63230c2b7b3012954d063b1ac4ba47e7c
irina-lapkina <infokotostudio@gmail.com>
2024-10-17 18:58:45 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+const zeroToNineteen = [
+    "",
+    " one",
+    " two",
+    " three",
+    " four",
+    " five",
+    " six",
+    " seven",
+    " eight",
+    " nine",
+    " ten",
+    " eleven",
+    " twelve",
+    " thirteen",
+    " fourteen",
+    " fifteen",
+    " sixteen",
+    " seventeen",
+    " eighteen",
+    " nineteen",
+];
+const ty = [
+    "",
+    "",
+    " twenty",
+    " thirty",
+    " forty",
+    " fifty",
+    " sixty",
+    " seventy",
+    " eighty",
+    " ninety",
+];
+
+module.exports = function toReadable(number) {
+    if (number == 0) {
+        return "zero";
+    }
+    if (number <= 19) {
+        return zeroToNineteen[number].trim();
+    }
+    if (20 <= number && number < 100) {
+        return (
+            ty[number.toString()[0]].trim() +
+            zeroToNineteen[number.toString()[1]]
+        );
+    }
+    if (number == 100) {
+        return "one hundred";
+    }
+    if (number > 100) {
+        const lastTwoDigits = number % 100;
+        const readableLastTwoDigits =
+            lastTwoDigits == 0 ? "" : toReadable(lastTwoDigits);
+        return (
+            zeroToNineteen[number.toString()[0]].trim() +
+            " hundred" +
+            (readableLastTwoDigits ? " " + readableLastTwoDigits : "")
+        );
+    }
+};
